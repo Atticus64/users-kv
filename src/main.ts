aefe2fa9307d284a2a57d4adf3cd0b4de4fcef4b
Jonathan <jonathanelian64@gmail.com>
2023-04-29 19:39:21 -0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono/mod.ts";
 import users from "./routes/users.ts";
+import { cors } from "hono/middleware/cors/index.ts";
 
 const app = new Hono();
+
+app.use("/", cors());
 
 app.get("/", (c) => {
   return c.text("turtle 🐢!");
--- test/main_test.ts
+++ test/main_test.ts
@@ -1,26 +1,28 @@
-import { assert, assertEquals } from "https://deno.land/std@0.185.0/testing/asserts.ts";
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.185.0/testing/asserts.ts";
 import app from "../src/main.ts";
 
-Deno.test('Example', async () => {
-	const res = await app.request('http://localhost:8000')
+Deno.test("Example", async () => {
+  const res = await app.request("http://localhost:8000");
 
-	console.log(res)
-	assertEquals(res.status, 200)
-	assertEquals(await res.text(), 'turtle 🐢!')
-})
+  console.log(res);
+  assertEquals(res.status, 200);
+  assertEquals(await res.text(), "turtle 🐢!");
+});
 
-Deno.test('Should return err if user does not valid', async () => {
-	const res = await app.request('http://localhost:8000/users', {
-		method: 'POST',
-		body: JSON.stringify({
-			"name": "Test",
-			"age": 20,
-			"langs": ["Python"],
-			"username": "test",
-			"role": "SUPER_USER"
-		})
-	})
+Deno.test("Should return err if user does not valid", async () => {
+  const res = await app.request("http://localhost:8000/users", {
+    method: "POST",
+    body: JSON.stringify({
+      "name": "Test",
+      "age": 20,
+      "langs": ["Python"],
+      "username": "test",
+      "role": "SUPER_USER",
+    }),
+  });
 
-	assertEquals(res.status, 400)
-
-})
+  assertEquals(res.status, 400);
+});
